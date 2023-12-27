5d7353fa70c50be97301690b5a30c6c1c1a7ddfa
Adil Khursheed <mughaladil571@gmail.com>
2023-12-28 03:53:53 +0500

    Add assigned issues to prisma schema

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,27 +1,29 @@
 "use client";
 import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";;
+import { Skeleton } from "@/app/components"
 
 const AssigneeSelect = () => {
-  const [users, setUsers] = useState<User[]>([]);
-
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const { data } = await axios.get<User[]>("/api/users");
-      setUsers(data);
-    };
-
-    fetchUsers();
+  const { data: users, error, isLoading } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000,
+    retry: 3,
   });
+
+  if(isLoading) return <Skeleton />
+
+  if (error) return null
+
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assign.." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          {users.map((user) => (
+          {users?.map((user) => (
             <Select.Item key={user.id} value={user.id}>
               {user.name}
             </Select.Item>
